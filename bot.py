c169762cd8b4412e2f6f149dcffb727b3d271ef4
Charlton Trezevant <7227500+chtzvt@users.noreply.github.com>
2023-08-28 14:47:43 -0400

    Upload chart images instead of just linking previews

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 """
     GEFS Chart Bot
-    Polls https://www.tropicaltidbits.com/storminfo/11L_gefs_latest.png, but it can
+    Polls NOAA's GEFS storm graphics, but it can
     really be used to monitor/notify about changes to any file on the web.
 
     (c) Charlton Trezevant 2017
@@ -10,16 +10,17 @@
 """
 
 import time, sys
+import requests, tempfile, os
 sys.dont_write_bytecode = True
 sys.path.insert(0, 'lib')
 
 from EtagMonitor import EtagMonitor
 from slack_sdk import WebClient
 
-CHART_URL = 'https://www.nhc.noaa.gov/storm_graphics/AT10/AL102023_5day_cone_no_line_and_wind.png' # replace with your storm path image url
-DB_PATH = '/tmp/etag.db'
-SLACK_TOKEN = 'your slack token here'
-SLACK_CHANNEL = 'your channel here'
+CHART_URL = 'https://www.nhc.noaa.gov/storm_graphics/AT10/AL102023_5day_cone_no_line_and_wind.png'
+DB_PATH = 'etag.db'
+SLACK_TOKEN = ''
+SLACK_CHANNEL = ''
 
 monitor = EtagMonitor(dbpath=DB_PATH, url=CHART_URL)
 slack = WebClient(SLACK_TOKEN)
@@ -27,8 +28,21 @@ slack = WebClient(SLACK_TOKEN)
 if monitor.has_updated() is True:
     curtime = time.strftime('%b %d, %Y at %H:%M')
     nocache = "?nocache=" + time.strftime('%d%H%M')
-    msg_text = 'Updated GEFS Chart: ' + curtime + '\n(NOAA)'
-    msg_attachments = [{"title": "Storm Track - Updated " + curtime, "image_url": CHART_URL + nocache}]
+    msg_text = 'Updated GEFS Chart: ' + curtime + ' (via NOAA)'
 
-    slack.chat_postMessage(channel=SLACK_CHANNEL,
-                   text=msg_text, attachments=msg_attachments)
+    temp = tempfile.NamedTemporaryFile(delete=False)
+
+    response = requests.get(CHART_URL)
+    response.raise_for_status()
+
+    open(temp.name, 'wb').write(response.content)
+
+    slack.files_upload_v2(
+        channel=SLACK_CHANNEL,
+        title=msg_text,
+        file=temp.name,
+        initial_comment=msg_text,
+    )
+
+    temp.close()
+    os.unlink(temp.name)
